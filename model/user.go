b5601bb5cc4f7c046d26705a6323269f0e7a544b
tamashiro-syuta <syuta5825@gmail.com>
2024-08-15 18:47:27 +0900

    🗃️ モデルを定義

---
+++ model/user.go
@@ -0,0 +1,17 @@
+// NOTE: Goではディレクトリがパッケージに対応する
+package model
+
+import "time"
+
+type User struct {
+	ID        unit 		  `json:"id" gorm:"primary_key"` // NOTE: jsonに変換時に自動的に小文字に変換させる
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserResponse struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Email     string    `json:"email" gorm:"unique"`
+}
